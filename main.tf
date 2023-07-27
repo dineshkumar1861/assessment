7cf9984899e8e1dbbefed09d3581b9192a1bd171
dineshkumar1861 <135444575+dineshkumar1861@users.noreply.github.com>
2023-07-27 14:49:40 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,7 +36,8 @@ resource "aws_instance" "web-server" {
         yum install -y docker
         service docker start
         docker pull bkimminich/juice-shop
-        docker run -d -p 80:3000 bkimminich/juice-shop      
+        docker run -d -p 80:3000 bkimminich/juice-shop
+        EOF
     tags = {
         Name = "instance-${count.index}"
     }
